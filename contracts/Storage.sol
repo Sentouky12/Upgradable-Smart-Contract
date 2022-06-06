595b9f9e544a97fd279778b6eeb0376620701e96
Sentouky12 <74101180+Sentouky12@users.noreply.github.com>
2022-06-06 03:03:41 +0300

    Update Storage.sol

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,6 +2,7 @@ pragma solidity 0.8.0;
 
 contract Storage {
   uint256 public i;
+  mapping (address => uint256) _addressx2storage;
   mapping (string => uint256) _uintStorage;
   mapping (string => address) _addressStorage;
   mapping (string => bool) _boolStorage;
